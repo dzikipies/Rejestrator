04d3f67020d1911f37298625b85737c7a6fdd24c
dzikipies <45237129+dzikipies@users.noreply.github.com>
2019-01-16 23:55:28 +0100

    Add files via upload

---
+++ Rejestrator/Rejestrator/Logic/FileLoader.cs
@@ -0,0 +1,99 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Rejestrator.Logic
+{
+    public class FileLoader
+    {
+        private string actualPath { get; set; }
+
+        public FileLoader()
+        {
+            actualPath = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            actualPath += @"\data";
+        }
+
+        public void PreparationFile()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string[] lines = File.ReadAllLines(openFileDialog.FileName);
+
+                string path = "";
+                foreach (var line in lines)
+                {
+                    if (!line.StartsWith("#"))
+                    {
+                        File.AppendAllText("data/" + path + ".txt", line);
+                    }
+                    else
+                    {
+                        path = line;
+                        File.AppendAllText("data/" + path + ".txt", path);
+                    }
+                    File.AppendAllText("data/" + path + ".txt", Environment.NewLine);
+                }
+            }
+        }
+
+        public SeriesCollection LoadFiles()
+        {
+            SeriesCollection SeriesCollection = new SeriesCollection();
+
+            string[] filePaths = Directory.GetFiles(actualPath, "*.txt", SearchOption.TopDirectoryOnly);
+
+            foreach (var file in filePaths)
+            {
+                ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+
+                string[] dataFile = File.ReadAllLines(file);
+
+                foreach (var line in dataFile)
+                {
+                    if (!line.StartsWith("#"))
+                    {
+                        var value = line.Split(' ');
+
+                        double x = 0.0;
+                        double y = 0.0;
+
+                        Double.TryParse(value[0], out x);
+                        Double.TryParse(value[1], out y);
+
+                        points.Add(new ObservablePoint
+                        {
+                            X = x,
+                            Y = y
+                        });
+                    }
+                    else
+                    {
+                        SeriesCollection.Add(new LineSeries
+                        {
+                            Title = line.Replace("#", ""),
+                            Values = points
+                        });
+                    }
+                }
+            }
+            return SeriesCollection;
+        }
+
+        public void RemoveFile()
+        {
+            DirectoryInfo dir = new DirectoryInfo(actualPath);
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
---
+++ Rejestrator/Rejestrator/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using LiveCharts;
+using Rejestrator.Logic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Rejestrator
+{
+    public partial class MainWindow : Window
+    {
+        public SeriesCollection SeriesCollection { get; set; }
+        private readonly FileLoader FileLoader;
+        public string NameX { get; set; }
+        public string NameY { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SeriesCollection = new SeriesCollection(); 
+            FileLoader = new FileLoader();
+        }
+
+        private void LoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileLoader.RemoveFile();
+            plotList.Items.Clear();
+            SeriesCollection.Clear();
+            DataContext = null;
+    
+            FileLoader.PreparationFile();
+            SeriesCollection = FileLoader.LoadFiles();
+
+            foreach(var item in SeriesCollection)
+                plotList.Items.Add(item.Title);
+
+            DataContext = this;
+        }
+
+        private void TextBox_TextChanged_X(object sender, TextChangedEventArgs e)
+        {
+            if (textX != null)
+                NameX = textX.Text.ToString();
+        }
+
+        private void TextBox_TextChanged_Y(object sender, TextChangedEventArgs e)
+        {
+            if (textY != null)
+                NameY = textY.Text.ToString();
+        }
+
+        private void PlotList_SelectionRemoved(object sender, SelectionChangedEventArgs e)
+        {
+            if (plotList.SelectedIndex >= 0)
+            {
+                SeriesCollection.RemoveAt(plotList.SelectedIndex);
+                plotList.Items.RemoveAt(plotList.Items.IndexOf(plotList.SelectedItem));
+            }
+        }
+    }
+}
+
